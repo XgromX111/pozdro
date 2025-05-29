b7f22505e5f94aaebe729afe9dc1ae2d980601f6
XgromX111 <kaledmahmis@gmail.com>
2025-05-29 20:39:18 +0200

    Fix email validation system

--- validate_email.php
+++ validate_email.php
@@ -20,15 +20,36 @@ $domain = substr(strrchr($email, "@"), 1);
 
 // Check if domain exists and has MX record
 if (!checkdnsrr($domain, 'MX')) {
-    echo json_encode(['valid' => false, 'error' => 'Domena email nie istnieje']);
+    echo json_encode(['valid' => false, 'error' => 'Domena email nie istnieje lub nie może odbierać wiadomości']);
     exit;
 }
 
+// List of disposable email domains - expanded list
+$disposableDomains = [
+    'tempmail.com', 'temp-mail.org', 'guerrillamail.com', 'throwawaymail.com',
+    'yopmail.com', 'mailinator.com', '10minutemail.com', 'trashmail.com',
+    'sharklasers.com', 'guerrillamail.info', 'grr.la', 'maildrop.cc',
+    'getairmail.com', 'getnada.com', 'emailondeck.com', 'tempmail.net',
+    'dispostable.com', 'tempmailaddress.com', 'emailfake.com', 'fakeinbox.com'
+];
+
 // Check for disposable email domains
-$disposableDomains = ['tempmail.com', 'temp-mail.org', 'guerrillamail.com'];
-if (in_array($domain, $disposableDomains)) {
+if (in_array(strtolower($domain), $disposableDomains)) {
     echo json_encode(['valid' => false, 'error' => 'Tymczasowe adresy email nie są dozwolone']);
     exit;
+}
+
+// Additional validation for common email providers
+$commonProviders = ['gmail.com', 'yahoo.com', 'outlook.com', 'hotmail.com', 'wp.pl', 'o2.pl', 'interia.pl', 'onet.pl'];
+$isCommonProvider = in_array(strtolower($domain), $commonProviders);
+
+// If domain is not a common provider, do additional checks
+if (!$isCommonProvider) {
+    // Check if domain has a valid A record (web server)
+    if (!checkdnsrr($domain, 'A')) {
+        echo json_encode(['valid' => false, 'error' => 'Domena wydaje się być nieprawidłowa']);
+        exit;
+    }
 }
 
 // If all checks pass
